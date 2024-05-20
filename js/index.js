380b57f29b35f108f50d480400cd07adfc2a0519
mercury-paws <145927427+mercury-paws@users.noreply.github.com>
2024-05-20 14:59:51 +0200

    final

--- js/index.js
+++ js/index.js
@@ -1,6 +1,9 @@
 const modal = document.querySelector(".backdrop-leave-comment-overlay");
 const leaveReviewBtn = document.querySelector(".review-btn");
 const modalCloseBtn = document.querySelector(".modal-close-btn");
+const mobileMenu = document.querySelector(".backdrop-mobile-menu");
+const mobileMenuCloseBtn = document.querySelector(".close-icon-button");
+const burgerBtn = document.querySelector(".burger-button");
 
 leaveReviewBtn.addEventListener("click", () => {
   modal.classList.toggle("is-open");
@@ -8,4 +11,12 @@ leaveReviewBtn.addEventListener("click", () => {
 
 modalCloseBtn.addEventListener("click", () => {
   modal.classList.toggle("is-open");
+});
+
+burgerBtn.addEventListener("click", () => {
+  mobileMenu.classList.toggle("is-open");
+});
+
+mobileMenuCloseBtn.addEventListener("click", () => {
+  mobileMenu.classList.toggle("is-open");
 });
--- js/swiper.js
+++ js/swiper.js
@@ -1,7 +1,7 @@
 const reviews = document.querySelectorAll(".review-item");
 const nextBtn = document.getElementById("nextBtn");
 const prevBtn = document.getElementById("prevBtn");
-
+const dots = document.querySelectorAll(".dot-item");
 let currentIndex = 0;
 
 function updateVisibility() {
@@ -9,13 +9,22 @@ function updateVisibility() {
     review.style.display = i === currentIndex ? "block" : "none";
   });
 }
-
+function updateDot() {
+  dots.forEach((dot, i) => {
+    if (i === currentIndex) {
+      dot.classList.add("dot-active");
+    } else {
+      dot.classList.remove("dot-active");
+    }
+  });
+}
 function nextReview() {
   currentIndex++;
   if (currentIndex > reviews.length - 1) {
     currentIndex = reviews.length - 1;
   }
   updateVisibility();
+  updateDot();
 }
 
 function prevReview() {
@@ -24,6 +33,7 @@ function prevReview() {
     currentIndex = 0;
   }
   updateVisibility();
+  updateDot();
 }
 
 nextBtn.addEventListener("click", nextReview);
@@ -31,3 +41,4 @@ prevBtn.addEventListener("click", prevReview);
 
 // Initially show the first review
 updateVisibility();
+updateDot();
